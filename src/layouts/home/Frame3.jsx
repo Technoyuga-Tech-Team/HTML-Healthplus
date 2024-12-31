4fb52754b27a6d387e2f05a8ebfd79ea59212344
nira-technoyuga <154118973+nira-technoyuga@users.noreply.github.com>
2024-12-31 18:01:21 +0530

    Frame 3 design

--- src/layouts/home/Frame3.jsx
+++ src/layouts/home/Frame3.jsx
@@ -1,10 +1,29 @@
 
+import ButtonComp from '@/componant/ButtonComp/ButtonComp'
+import { LANDING_PAGE_IMAGES } from '@/constant/images'
+import { imagesForGrid } from '@/constant/staticData'
 import React from 'react'
 
 const Frame3 = () => {
   return (
-    <div>Frame3</div>
-  )
+    <div className='container'>
+      <div className='tf-main-wrapper'>
+        <div className='tf-main-wrapper-left'>
+        <img src={LANDING_PAGE_IMAGES[5]} />
+        </div>
+        <div className='tf-main-wrapper-right'>
+          <div className='my-3 dark-font font-size1 font-color1 line-height1 '>
+            About Us
+          </div>
+          <div className='my-3 regular-font font-size2 font-color2 fw-normal' >At <span style={{color:"var(--primary-color)"}}>interp,</span> we’re dedicated to supporting health entrepreneurs who dare to dream big. Whether you're developing breakthrough medical technologies, launching wellness programs, or innovating in patient care, we provide the resources, mentorship, and connections you need to succeed.</div>
+          <div className='my-4'>
+            <ButtonComp
+              text={"Book Appointment"}
+            />
+          </div>
+        </div>
+      </div>
+    </div>)
 }
 
 export default Frame3
--- src/layouts/home/index.jsx
+++ src/layouts/home/index.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Frame1 from './Frame1'
 import './index.css'
 import Frame2 from './Frame2'
+import Frame3 from './Frame3'
 const Home = () => {
   return (
     <>
     <Frame1/>
     <Frame2 />
+    <Frame3 />
     </>
   )
 }
